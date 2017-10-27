ab605bed818383b3d9c6f56a7017f187a708c6fb
Laura Meerkatz <lmeerkatz@salesforce.com>
2017-10-27 09:12:03 -0700

    Add option to use location

--- src/aura/RecommendedSessions/RecommendedSessionsController.js
+++ src/aura/RecommendedSessions/RecommendedSessionsController.js
@@ -1,13 +1,17 @@
 ({
-	doInit : function(component, event, helper) {
-		var action = component.get("c.getRecommendations");
-        action.setParams({
-            contactId : component.get("v.recordId")
-        });
-        action.setCallback(this, function(response){
-            var recs = response.getReturnValue().recommendations;
-            component.set("v.sessionList", recs.slice(0, 3));
-        });
-        $A.enqueueAction(action);
-	}
+  doInit: function (component, event, helper) {
+    var action = component.get("c.getRecommendations");
+
+    action.setParams({
+      contactId: component.get("v.recordId"),
+      useLocation: component.get("v.useLocation")
+    });
+
+    action.setCallback(this, function (response) {
+      var recs = response.getReturnValue().recommendations;
+      component.set("v.sessionList", recs.slice(0, 3));
+      console.log(recs);
+    });
+    $A.enqueueAction(action);
+  }
 })
